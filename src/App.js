7b5aaf4151a5be856080938cce47577d65d83b95
Sarah <vanwars@gmail.com>
2023-11-03 13:33:42 -0400

    simplified main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
 
 function App() {
-    const [restaurants, setRestaurants] = useState([]);
-    // fetching data and use effects
-    async function fetchRestaurants() {
-        const baseURL =
-            "https://www.apitutor.org/yelp/simple/v3/businesses/search?";
-        const url = `${baseURL}location=Asheville,%20NC&term=pizza&limit=5`;
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-        setRestaurants(data);
-    }
-
-    useEffect(() => {
-        fetchRestaurants();
-    }, []);
-
     return (
         <div className="App">
             <header>Header</header>
--- src/js-notes.js
+++ src/js-notes.js
@@ -1,33 +1,36 @@
-// fetching data and use effects
-async function fetchRestaurants() {
-    const baseURL =
-        "https://www.apitutor.org/yelp/simple/v3/businesses/search?";
-    const url = `${baseURL}location=Asheville,%20NC&term=pizza&limit=5`;
-    const response = await fetch(url);
-    const restaurants = await response.json();
-    console.log(restaurants);
-}
+// <form>
+//     <h2>Search for Restaurant</h2>
+//     <label>Location:</label>
+//     <input type="text" placeholder="Location" />
+//     <label className="cuisine">Cuisine:</label>
+//     <input type="text" placeholder="Type of cuisine" />
+//     <button>Search</button>
+// </form>;
 
-useEffect(() => {
-    fetchRestaurants();
-}, []);
+// import React, { useState, useEffect } from "react";
 
-<form>
-    <h2>Search for Restaurant</h2>
-    <label>Location:</label>
-    <input type="text" placeholder="Location" />
-    <label className="cuisine">Cuisine:</label>
-    <input type="text" placeholder="Type of cuisine" />
-    <button>Search</button>
-</form>;
+// // fetching data and use effects
+// const [restaurants, setRestaurants] = useState([]);
+// async function fetchRestaurants() {
+//     const baseURL =
+//         "https://www.apitutor.org/yelp/simple/v3/businesses/search?";
+//     const url = `${baseURL}location=Asheville,%20NC&term=pizza&limit=5`;
+//     const response = await fetch(url);
+//     const restaurants = await response.json();
+//     console.log(restaurants);
+// }
 
-{
-    restaurants.map((restaurant) => {
-        return (
-            <div className="restaurant">
-                <img src={restaurant.image_url} />
-                <p>{restaurant.name}</p>
-            </div>
-        );
-    });
-}
+// useEffect(() => {
+//     fetchRestaurants();
+// }, []);
+
+// {
+//     restaurants.map((restaurant) => {
+//         return (
+//             <div className="restaurant">
+//                 <img src={restaurant.image_url} />
+//                 <p>{restaurant.name}</p>
+//             </div>
+//         );
+//     });
+// }
